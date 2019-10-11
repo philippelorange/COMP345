ed6ecfe2c997b2ea2ec8af513aca80679b05ab48
Alec Adub <adub.alec@gmail.com>
2019-10-11 15:30:49 -0400

    Fix extra-files and finish cards

--- Cards/CardsDriver.cpp
+++ Cards/CardsDriver.cpp
@@ -1,13 +1,98 @@
 #include "Cards/Cards.h"
 #include <iostream>
+#include <Map.h>
 
 int main() {
+    //[continents]
+    auto* continent1 = new Continent("Maritimes");
+    auto* continent2 = new Continent("Quebec");
+    auto* continent3 = new Continent("Ontario");
+
+    auto* continents = new vector<Continent*>;
+    continents->push_back(continent1);
+    continents->push_back(continent2);
+    continents->push_back(continent3);
+
+    //[countries]
+    auto* c1 = new Country("NB", continent1);
+    auto* c2 = new Country("PEI", continent1);
+    auto* c3 = new Country("NS", continent1);
+    auto* c4 = new Country("Newfoundland", continent1);
+    auto* c5 = new Country("Labrador", continent1);
+
+    auto* c6 = new Country("QC-North", continent2);
+    auto* c7 = new Country("QC-Central", continent2);
+    auto* c8 = new Country("QC-South", continent2);
+
+    auto* c9 = new Country("Ottawa", continent3);
+
+    auto* countries = new vector<Country*>;
+    countries->push_back(c1);
+    countries->push_back(c2);
+    countries->push_back(c3);
+    countries->push_back(c4);
+    countries->push_back(c5);
+    countries->push_back(c6);
+    countries->push_back(c7);
+    countries->push_back(c8);
+    countries->push_back(c9);
+
+    continent1->add_country(c1);
+    continent1->add_country(c2);
+    continent1->add_country(c3);
+    continent1->add_country(c4);
+    continent1->add_country(c5);
+    //continent1->add_country(c6); //Test case 3: Uncomment this. (Adds a country to a second continent)
+
+    continent2->add_country(c6);
+    continent2->add_country(c7);
+    continent2->add_country(c8);
+
+    continent3->add_country(c9);
+
+    //[borders]
+    c1->add_adjacent_country(c2);
+    c1->add_adjacent_country(c3);
+    c1->add_adjacent_country(c8);
+
+    c2->add_adjacent_country(c1);
+    c2->add_adjacent_country(c3);
+
+    c3->add_adjacent_country(c1);
+    c3->add_adjacent_country(c2);
+    c3->add_adjacent_country(c4);
+
+    c4->add_adjacent_country(c3);
+    c4->add_adjacent_country(c5);
+    c4->add_adjacent_country(c7);
+
+    c5->add_adjacent_country(c4);
+    c5->add_adjacent_country(c6);
+    c5->add_adjacent_country(c7);
+
+    c6->add_adjacent_country(c5);
+    c6->add_adjacent_country(c7); //Test case 2: Comment this out. (Breaks QC-North off of the rest of Quebec, forces you to pass by Maritimes, invalid)
+
+    c7->add_adjacent_country(c4);
+    c7->add_adjacent_country(c5);
+    c7->add_adjacent_country(c6); //Test case 2: Comment this out. (Breaks QC-North off of the rest of Quebec, forces you to pass by Maritimes, invalid)
+    c7->add_adjacent_country(c8);
+
+    c8->add_adjacent_country(c7);
+    c8->add_adjacent_country(c9); //Test case 1: Comment this out. (Breaks Ontario off of Quebec)
+
+    c9->add_adjacent_country(c8); //Test case 1: Comment this out. (Breaks Ontario off of Quebec)
+
+    //Create map
+    Map* map = new Map(continents, countries);
+
+
     auto* cards = new vector<Card*>;
     srandom(time(nullptr)); // random seed to use unpredictable numbers
 
     cout << "Creating cards.." << endl;
 
-    int nbr_cards = 42; // TODO: get number of cards from map instead of 42 (hardcoded)
+    int nbr_cards = map->get_countries()->size();
 
     while (nbr_cards % 3 != 0) { // get number divisible by 3 for purpose of this assignment
         nbr_cards--;
